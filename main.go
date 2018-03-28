d5bc9996fe577a1ea1e23f234c50eef59bea9d87
Shota Nemoto <shota.nemoto@gmail.com>
2018-03-28 21:13:02 +0900

    add handler to execute chatroom

--- main.go
+++ main.go
@@ -23,10 +23,15 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
 
+	// start chatroom
+	go r.run()
+
+	// run web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
-
 }
